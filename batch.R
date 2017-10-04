e4fb583d988394c30f51edcfc20f13b3d83b6cd7
depthgr8 <depthgr8@gmail.com>
2017-10-05 01:15:36 +0530

    Generate Excel files using xlsx package and .TXT files using cat

--- batch.R
+++ batch.R
@@ -3,7 +3,7 @@
 
 # Read file and folders ---------------------------------------------------
 
-setwd("~/Documents/depp/Excel_batch_generator")
+setwd("~/Excel_batch_generator")
 files <-  Sys.glob("*.bvec")
 limit <- length(files)
 list.index <- 1:limit
@@ -18,7 +18,22 @@ rownames(di) <- NULL
 
 # Generate Excel sheets ---------------------------------------------------
 
+write.xlsx(as.matrix(di[1:30,]),file="1.xls",sheetName = "1",row.names = FALSE,col.names = FALSE)
+write.xlsx(as.matrix(di[31:60,]),file="2.xls",sheetName = "1",row.names = FALSE,col.names = FALSE)
+write.xlsx(as.matrix(di[61:90,]),file="3.xls",sheetName = "1",row.names = FALSE,col.names = FALSE)
+write.xlsx(as.matrix(di[91:120,]),file="4.xls",sheetName = "1",row.names = FALSE,col.names = FALSE)
+
+# Generate CSV files ------------------------------------------------------
+
 write.csv(di[1:30,],file = "1.csv",row.names = FALSE)
 write.csv(di[31:60,],file = "2.csv",row.names = FALSE)
 write.csv(di[61:90,],file = "3.csv",row.names = FALSE)
 write.csv(di[91:120,],file = "4.csv",row.names = FALSE)
+
+# Generate text files -----------------------------------------------------
+
+cat(as.matrix(di[1:30,]),file = "1.txt",sep = " ")
+cat(as.matrix(di[31:60,]),file = "2.txt",sep = " ")
+cat(as.matrix(di[61:90,]),file = "3.txt",sep = " ")
+cat(as.matrix(di[91:120,]),file = "4.txt",sep = " ")
+
